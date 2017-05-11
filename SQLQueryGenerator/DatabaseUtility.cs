88f40ccb684f17070792561d31496734599b6ac5
Pratik Jaisinghani <pratikj@ESDP156>
2017-05-11 10:38:41 +0530

    MDI Container Added to expand an application

---
+++ SQLQueryGenerator/DatabaseUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLQueryGenerator
+{
+    public partial class DatabaseUtility : Form
+    {
+        public DatabaseUtility()
+        {
+            InitializeComponent();
+            //SetDefault();
+        }
+
+        private void scriptGeneratorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SQLScriptGenerator scriptGenerator = new SQLScriptGenerator();
+            scriptGenerator.MdiParent = this;
+            scriptGenerator.Show();
+        }
+
+        private void SetDefault()
+        {
+            SQLScriptGenerator scriptGenerator = new SQLScriptGenerator();
+            scriptGenerator.MdiParent = this;
+            scriptGenerator.Show();
+        }
+    }
+}
